14bac45cf8a3f26850c0cdb2cea3ab05f8d1a3cc
19014273 <19014273@CHENG-PC>
2020-07-21 13:48:50 +0800

    DoLoan

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -94,11 +94,26 @@ public class ResourceCentreTest {
 	}
 
 	@Test
-	public void doLoanCamcorderTest() {
+	public boolean doLoanCamcorderTest(ArrayList<Camcorder> camcorderList, String tag, String dueDate) {
 		//fail("Not yet implemented");
 		// write your code here
+		boolean isLoaned = false;
+
+		for (int i = 0; i < camcorderList.size(); i++) {
+			if (tag.equalsIgnoreCase(camcorderList.get(i).getAssetTag())
+					&& camcorderList.get(i).getIsAvailable() == true) {
+				
+				camcorderList.get(i).setIsAvailable(false);
+				camcorderList.get(i).setDueDate(dueDate);
+				
+				isLoaned = true;
+			}
+			}
+			return isLoaned;
+		}
+	
 		
-	}
+	
 	
 	@Test
 	public void doLoanChromebookTest() {
